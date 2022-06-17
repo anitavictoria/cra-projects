fd7d4bca53e7f06f080558fd3babafc24a4fb2c2
Anita <ahlistopad@gmail.com>
2022-06-18 00:03:34 +0200

    styling and adding Word component to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,42 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Word from "./Word";
 
+class App extends Component {
+  state = {
+    words: [],
+    isLoaded: false,
+  };
 
+  componentDidMount() {
+    fetch("data/words.json")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          words: data.words,
+          isLoaded: true,
+        });
+      });
+  }
 
+  // fetchData = () => {
+  //   fetch('data/words.json')
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       this.setState({
+  //         words: data.words,
+  //         isLoaded: true
+  //       })
+  //     })
+  // }
 
+  render() {
+    console.log("render");
+    const words = this.state.words.map((word) => (
+      <Word key={word.id} english={word.en} polish={word.pl} />
+    ));
+    return <ul>{this.state.isLoaded ? words : "Wczytuję dane"}</ul>;
+  }
+}
 
 export default App;
