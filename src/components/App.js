a0fe875573190f95b0b393f54042373fc5199a4c
Anita <ahlistopad@gmail.com>
2022-06-18 18:58:34 +0200

    using XMLHttpRequest

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,30 @@
 import React, { Component } from "react";
 import "./App.css";
-import Word from "./Word";
 
 class App extends Component {
   state = {
-    words: [],
-    isLoaded: false,
+    users: [],
   };
 
   componentDidMount() {
-    fetch("data/words.json")
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({
-          words: data.words,
-          isLoaded: true,
-        });
-      });
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        const users = JSON.parse(xhr.response);
+        this.setState({ users });
+      }
+    };
+    xhr.send(null);
   }
-
-  // fetchData = () => {
-  //   fetch('data/words.json')
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       this.setState({
-  //         words: data.words,
-  //         isLoaded: true
-  //       })
-  //     })
-  // }
-
   render() {
-    console.log("render");
-    const words = this.state.words.map((word) => (
-      <Word key={word.id} english={word.en} polish={word.pl} />
+    const users = this.state.users.map((user) => (
+      <div key={user.id}>
+        <h4>{user.name}</h4>
+        <p>{user.address.city}</p>
+      </div>
     ));
-    return <ul>{this.state.isLoaded ? words : "Wczytuję dane"}</ul>;
+    return <div>{users}</div>;
   }
 }
 
