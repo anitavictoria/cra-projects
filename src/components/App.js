a7d7f121a275e6d7da1b609a6c26a53767e724c8
Anita <ahlistopad@gmail.com>
2022-06-22 00:36:07 +0200

    form-with-validation

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,10 @@
-import React, { Component } from "react";
-import "./App.css";
-import UsersList from "./UsersList";
-import ButtonFetchUsers from "./ButtonFetchUsers";
+import React from 'react'
 
-const API = "https://randomuser.me/api/?results=5";
-class App extends Component {
-  state = {
-    users: [],
-  };
-
-  handleDataFetch = () => {
-    fetch(API)
-      .then((response) => {
-        if (response.ok) {
-          // console.log(response);
-          return response;
-        }
-        throw Error(response.status);
-      })
-      .then((response) => response.json())
-      .then((data) => {
-        const user = data.results;
-
-        this.setState((prevState) => ({
-          users: prevState.users.concat(user),
-        }));
-      })
-      .catch((error) => console.log(error + " coś nie tak"));
-  };
-
-  render() {
-    const users = this.state.users;
-    return (
-      <div>
-        <ButtonFetchUsers click={this.handleDataFetch} />
-        {users.length > 0 ? <UsersList users={users} /> : users}
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
+
