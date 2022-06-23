c8acdbd719b25ba9b8c782cca79722fc3911efb3
Anita <ahlistopad@gmail.com>
2022-06-23 12:12:10 +0200

    added error messages and completed validation

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,24 @@ class App extends Component {
     email: "",
     password: "",
     accept: false,
+    message: "",
+
+    errors: {
+      username: false,
+      email: false,
+      password: false,
+      accept: false,
+    },
   };
+
+  messages = {
+    username_incorrect:
+      "Nazwa musi być dłuższa niż 10 znaków i nie może zawierać spacji",
+    email_incorrect: "Adres email musi zawierać znak @",
+    password_incorrect: "Hasło musi mieć dokładnie 8 znaków",
+    accept_incorrect: "Nie potwierdzono zgody",
+  };
+
   handleChange = (e) => {
     const name = e.target.name;
     const type = e.target.type;
@@ -25,9 +42,73 @@ class App extends Component {
   };
 
   handleSubmit = (e) => {
+    const validaton = this.formValidation();
     e.preventDefault();
     console.log("ok");
+    if (validaton.correct) {
+      this.setState({
+        username: "",
+        email: "",
+        password: "",
+        accept: false,
+        message: "Formularz wysłany",
+      });
+    } else {
+      this.setState({
+        errors: {
+          username: !validaton.username,
+          email: !validaton.email,
+          password: !validaton.password,
+          accept: !validaton.accept,
+        },
+      });
+    }
   };
+
+  formValidation = () => {
+    let username = false;
+    let email = false;
+    let password = false;
+    let accept = false;
+    let correct = false;
+    if (
+      this.state.username.length > 10 &&
+      this.state.username.indexOf(" ") === -1
+    ) {
+      username = true;
+    }
+    if (this.state.email.indexOf("@") !== -1) {
+      email = true;
+    }
+    if (this.state.password.length === 8) {
+      password = true;
+    }
+    if (this.state.accept) {
+      accept = true;
+    }
+    if (username && email && password && accept) {
+      correct = true;
+    }
+    return {
+      correct,
+      username,
+      email,
+      password,
+      accept,
+    };
+  };
+
+  componentDidUpdate() {
+    if (this.state.message !== "") {
+      setTimeout(
+        () =>
+          this.setState({
+            message: "",
+          }),
+        3000
+      );
+    }
+  }
 
   render() {
     return (
@@ -43,6 +124,11 @@ class App extends Component {
               value={this.state.username}
               onChange={this.handleChange}
             ></input>
+            <span>
+              {this.state.errors.username && (
+                <span>{this.messages.username_incorrect}</span>
+              )}
+            </span>
           </label>
 
           <label htmlFor="email">
@@ -54,7 +140,13 @@ class App extends Component {
               name="email"
               value={this.state.email}
               onChange={this.handleChange}
-            ></input>
+            >
+              <span>
+                {this.state.errors.email && (
+                  <span>{this.messages.email_incorrect}</span>
+                )}
+              </span>
+            </input>
           </label>
 
           <label htmlFor="password">
@@ -66,7 +158,14 @@ class App extends Component {
               name="password"
               value={this.state.password}
               onChange={this.handleChange}
-            ></input>
+            >
+              {" "}
+              <span>
+                {this.state.errors.password && (
+                  <span>{this.messages.password_incorrect}</span>
+                )}
+              </span>
+            </input>
           </label>
           <label htmlFor="accept">
             <input
@@ -75,12 +174,17 @@ class App extends Component {
               name="accept"
               checked={this.state.accept}
               onChange={this.handleChange}
-            >
-              wyrażam zgodę lorem ipsum
-            </input>
+            ></input>
+            wyrażam zgodę lorem ipsum
           </label>
+          <span>
+            {this.state.errors.accept && (
+              <span>{this.messages.accept_incorrect}</span>
+            )}
+          </span>
           <button>wyślij formularz</button>
         </form>
+        {this.state.message && <h3>{this.state.message}</h3>}
       </div>
     );
   }
